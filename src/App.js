8e87b53aa3c0a935ec53cc956077e1c800e9dfd0
telkomdev-pahuger <hughdev101@gmail.com>
2020-07-17 19:29:55 +0700

    App useEffect apply and etc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 
 export default function App() {
+  const [data, setData] = useState({
+    users: [],
+    loading: false,
+  });
+
+  // ComponentDidMount alike
+  useEffect(() => {
+    setData({
+      loading: true,
+    });
+
+    async function fetchData() {
+      const res = await axios.get('https://api.github.com/users');
+      setData({ users: res.data, loading: false });
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className='app-wrapper'>
       <Navbar icon='fab fa-github' title='Github Finder' />
