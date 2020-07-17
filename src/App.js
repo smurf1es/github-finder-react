fb4a0f8af1908455ad4be7614fd44cef3b2f8e2e
telkomdev-pahuger <hughdev101@gmail.com>
2020-07-17 19:47:59 +0700

    rollback to class based component on app.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
 import './App.css';
 
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 
-export default function App() {
-  const [data, setData] = useState({
+export default class App extends React.Component {
+  // const [data, setData] = useState({
+  //   users: [],
+  //   loading: false,
+  // });
+
+  // // ComponentDidMount alike
+  // useEffect(() => {
+  //   setData({
+  //     loading: true,
+  //   });
+
+  //   async function fetchData() {
+  //     const res = await axios.get('https://api.github.com/users');
+  //     setData({ users: res.data, loading: false });
+  //   }
+  //   fetchData();
+  // }, []);
+
+  state = {
     users: [],
     loading: false,
-  });
+  };
 
-  // ComponentDidMount alike
-  useEffect(() => {
-    setData({
-      loading: true,
-    });
-
-    async function fetchData() {
-      const res = await axios.get('https://api.github.com/users');
-      setData({ users: res.data, loading: false });
-    }
-    fetchData();
-  }, []);
-
-  return (
-    <div className='app-wrapper'>
-      <Navbar icon='fab fa-github' title='Github Finder' />
-      <Users />
-    </div>
-  );
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+  }
+  render() {
+    return (
+      <div className='app-wrapper'>
+        <Navbar icon='fab fa-github' title='Github Finder' />
+        <Users loading={this.state.loading} users={this.state.users} />
+      </div>
+    );
+  }
 }
